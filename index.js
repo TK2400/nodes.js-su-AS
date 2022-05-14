bbbaf81c13d436aa27294a1a4b37ca54f72fed90
T.Kav <tomas@plienas.lt>
2022-05-14 14:07:11 +0300

    padaryta uzduotis su knygu masyvu from to, sudedant jas i nauja masyva

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-// Sukurti atskirą "/books/:id"  maršrutą, kuris iš serverio grąžina pasirinktos knygos pavadinimą.
-
+//Sukurti atskirą "/books/:from/:to" maršrutą, kuris iš serverio grąžina JSON masyvą pasirinktų knygų.
 
 const express = require('express');
 const app = express();
 
 
 const knygos = [
-    "tadas blinda",
-    "biblija",
-    "senis ir jura"
+    "tadas blinda ",
+    "biblija ",
+    "senis ir jura ",
+    "siuvejo kronikos "
 ]
+
+console.log(knygos.slice(0, 3))
 
 app.listen(9000, () => {
     console.log(`Serveris paleistas. Laukia užklausų`);
 });
 
-app.get('/books/:id', (request, response) => {
-    const knygosId = request.params.id
-    response.json(knygos[knygosId])
+app.get('/books/:from/:to', (request, response) => {
+    const fromIndex = Number(request.params.from)
+    const fromTo = Number(request.params.to)
+    const atgnybtasMasyvas = knygos.slice(fromIndex, fromTo + 1)
+
+    response.json(atgnybtasMasyvas)
 
 })
