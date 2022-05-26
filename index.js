ffc6dbcbf19b440ed8ac5bf0c72fb8eaf8c3b2ef
T.Kav <tomas@plienas.lt>
2022-05-26 20:29:54 +0300

    uzkurta duombaze Mogodb knygu-projektas.knygos ; get metodas, kad gauti visas; insertOne metodas; veikiantis kodas

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = 'mongodb+srv://TK:TK2400@cluster0.y5udm.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 console.log(process.env.PORT);// taip pasiekiami aplinkos kintamieji
 
 const knygos = [
@@ -13,16 +22,28 @@ const knygos = [
   'siuvejo kronikos ',
 ];
 
-// console.log(knygos.slice(0, 3));
-
 app.listen(process.env.PORT, () => {
   console.log('Serveris paleistas. Laukia užklausų');
 });
 
-app.get('/book/:from/:to', (request, response) => {
-  const fromIndex = Number(request.params.from);
-  const fromTo = Number(request.params.to);
-  const atgnybtasMasyvas = knygos.slice(fromIndex, fromTo + 1);
+// app.get('/book/:from/:to', (request, response) => {
+//   const fromIndex = Number(request.params.from);
+//   const fromTo = Number(request.params.to);
+//   const atgnybtasMasyvas = knygos.slice(fromIndex, fromTo + 1);
 
-  response.json(atgnybtasMasyvas);
+//   response.json(atgnybtasMasyvas);
+// });
+
+app.get('/books', (req, response) => {
+  client.connect(async () => {
+    const collection = client.db('knygu-projektas').collection('knygos');
+    // const result = await collection.insertOne({
+    //   name: 'Rytas perone',
+    //   pages: 150,
+    // });
+    // console.log(result);
+    const anotherResult = await collection.find({}).toArray();
+    response.json(anotherResult);
+    client.close();
+  });
 });
